788b8d378c4da34f90e9bb46136558e469f869ee
unknown <denismaanga99@gmail.com>
2022-03-17 00:21:19 +0300

    My commit message

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,27 +10,20 @@
 
 int largest_number(int a, int b, int c)
 {
-int largest;
+	int largest;
 
-if (a > b)
-{
-if (a > c)
-{
-largest = a;
-}
-else
-{
-largest = c;
-}
-}
-else if (b > c)
-{
-largest = b;
-}
-else
-{
-largest = c;
-}
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
 
-return (largest);
+	return (largest);
 }
