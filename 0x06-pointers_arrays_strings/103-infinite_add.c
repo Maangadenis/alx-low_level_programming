de6fc7f98c5d1de87e5c4e4975535bc5d3b030c0
unknown <denismaanga99@gmail.com>
2022-03-23 22:45:36 +0300

    infinite

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,7 +9,7 @@
  *
  * Return: address of r or 0
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 {
 	int i, j, k, l, m, n;
 
