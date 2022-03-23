75df68e8c986c03feb3f7a586fb53053ef5111d4
unknown <denismaanga99@gmail.com>
2022-03-23 22:01:46 +0300

    Cat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  *_strncat - function that concatenates two strings. it will use
  * at most n bytes from src. src does not need to be null terminated.
