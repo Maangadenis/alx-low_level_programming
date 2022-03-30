7820083fc893fbedd1e36b3d8edba8106f48e920
unknown <denismaanga99@gmail.com>
2022-03-30 13:01:35 +0300

    puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,14 +5,13 @@
  * @s: input
  * Return: Always 0 (Success)
  */
-
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-	{
 		_putchar('\n');
-		return;
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
 }
