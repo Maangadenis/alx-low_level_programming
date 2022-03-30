6dc319c83e654ea7e6f8a468f9508ae893321c7b
unknown <denismaanga99@gmail.com>
2022-03-30 12:59:49 +0300

    puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Entry Point
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
