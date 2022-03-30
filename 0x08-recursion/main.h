38e3053e838e1585f1fabf09599cd0f74d0c0616
unknown <denismaanga99@gmail.com>
2022-03-30 20:08:09 +0300

    Main

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -10,5 +7,3 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
-
-#endif /* MAIN.H *
