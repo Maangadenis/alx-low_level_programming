54a191f5aba162a96478f3681f4acf1b0fbd23ff
unknown <denismaanga99@gmail.com>
2022-04-04 14:57:08 +0300

    start

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,49 +2,42 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
-  *str_concat - concatenates 2 strings.
-  * a NULL string is treated as an empty string.
-  *@s1: pointer to string.
-  *@s2: pointer to string.
-  *
-  *Return: pointer to newly allocated memory which
-  *has s1, s2 and null byte.
-  *NULL on failure.
-  */
+ * str_concat - concatenates 2 strings
+ *
+ * @s1: first string
+ * @s2: string to add to end of of first string
+ *
+ * Return: pointer to newly allocated string concatenation
+ */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int len1, len2, size, i, j;
-	char *nstr;
+	unsigned int size1 = 0, size2 = 0;
+	char *ptr, *ret;
 
-	if (s1 == NULL)
+	ptr = s1;
+	if (s1)
+		while (*ptr++)
+			size1++;
+	else
 		s1 = "";
-	if (s2 == NULL)
+
+	ptr = s2;
+	if (s2)
+		while (*ptr++)
+			size2++;
+	else
 		s2 = "";
 
-	len1 = 0;
-	while (s1[len1] != '\0')
-		len1++;
-	len2 = 0;
-	while (s2[len2] != '\0')
-		len2++;
-
-	size = len1 + len2;
-
-	nstr = malloc((sizeof(char) * size) + 1);
-	/*check if malloc was successful */
-	if (nstr == NULL)
+	ret = malloc(size1 + size2 + 1);
+	if (!ret)
 		return (NULL);
 
-	i = 0;
-	while (i < len1)
-	{
-		nstr[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (i <= size)
-	{
-		nstr[i] = s2[j];
-		i++;
-		j++;
+	ptr = ret;
+	while (*s1)
+		*ptr++ = *s1++;
+	while (*s2)
+		*ptr++ = *s2++;
+	*ptr = 0;
 
+	return (ret);
+}
